4fff5c6b45c1416a67e0bae662af9b161720003e
Duwan Sierra <desierrag@gmail.com>
2023-03-31 20:51:30 -0500

    test rollupOptions

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,14 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     outDir:  './docs',
-    site: 'https://duwansierra.github.io'
+    site: 'https://duwansierra.github.io',
+    vite: {
+        build: {
+          rollupOptions: {
+            output: {
+              assetFileNames: '[name][extname]',
+            }
+          }
+        }
+      }
 });
